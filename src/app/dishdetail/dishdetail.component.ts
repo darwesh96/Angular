ed1311f21af4200ccdfd83b235353d5f3a3e6382
mohamed darwesh <mohamed_darwesh123@hotmail.com>
2019-03-19 15:00:23 +0200

    HTTP Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -20,6 +20,7 @@ export class DishdetailComponent implements OnInit {
   dishIds : string[];
   prev: string;
   next:string;
+  errMess: string;
 
   commentForm: FormGroup;
   comment : Comment;
@@ -51,7 +52,8 @@ export class DishdetailComponent implements OnInit {
    this.dishService.getDishIds()
    .subscribe((dishIds) => this.dishIds = dishIds);
    this.route.params.pipe(switchMap((params:Params) => this.dishService.getDish(params['id'])))
-    .subscribe(dish => {this.dish = dish; this.setPrevNext(dish.id)});
+    .subscribe(dish => {this.dish = dish; this.setPrevNext(dish.id); },
+    errmess => this.errMess = <any>errmess);
   }
 
   createForm(): void {
