8d8ff94a0401004c57a30927745e244f7d9623f1
mohamed darwesh <mohamed_darwesh123@hotmail.com>
2019-03-19 14:28:00 +0200

    HTTP Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import {MatSliderModule} from '@angular/material/slider';
+import { HttpClientModule } from '@angular/common/http';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
@@ -33,7 +34,7 @@ import { LeaderService } from './services/leader.service';
 
 import {AppRoutingModule} from './app-routing/app-routing.module';
 import { LoginComponent } from './login/login.component';
-
+import { baseURL } from './shared/baseurl';
 
 @NgModule({
   declarations: [
@@ -67,12 +68,14 @@ import { LoginComponent } from './login/login.component';
     ReactiveFormsModule,
     MatProgressSpinnerModule,
     MatSliderModule,
+    HttpClientModule,
 
   ],
   providers: [
     DishService,
     PromotionService,
     LeaderService,
+    {provide: 'BaseURL', useValue: baseURL}
   ],
 
   entryComponents: [
