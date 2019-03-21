46a965eba3641fdaf1e0bc72c8d077392ee3f864
mohamed darwesh <mohamed_darwesh123@hotmail.com>
2019-03-21 16:32:29 +0200

    Directives

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -21,7 +21,7 @@ export class DishdetailComponent implements OnInit {
   prev: string;
   next:string;
   errMess: string;
-
+  dishcopy:Dish;
   commentForm: FormGroup;
   comment : Comment;
   formErrors = {
@@ -52,7 +52,7 @@ export class DishdetailComponent implements OnInit {
    this.dishService.getDishIds()
    .subscribe((dishIds) => this.dishIds = dishIds);
    this.route.params.pipe(switchMap((params:Params) => this.dishService.getDish(params['id'])))
-    .subscribe(dish => {this.dish = dish; this.setPrevNext(dish.id); },
+    .subscribe(dish => {this.dish = dish; this.dishcopy =dish; this.setPrevNext(dish.id); },
     errmess => this.errMess = <any>errmess);
   }
 
@@ -115,8 +115,14 @@ export class DishdetailComponent implements OnInit {
     var d = new Date();
     var n = d.toISOString();
     this.comment.date = n;
-    this.dish.comments.push(this.comment);
-    
+    this.dishcopy.comments.push(this.comment);
+    this.dishService.putDish(this.dishcopy)
+      .subscribe(dish => {
+        this.dish = dish; this.dishcopy = dish;
+      },
+        errmess => { this.dish = null; this.dishcopy = null;
+           this.errMess= <any>errmess;}
+      );
   }
 
 }
